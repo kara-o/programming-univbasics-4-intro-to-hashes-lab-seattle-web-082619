305b1fef93ec18e94b5ba978fca01e04130e0006
Kara Odle <karajo.odle@gmail.com>
2019-08-24 21:03:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,6 +20,7 @@ end
 def my_hash_creator(key, value)
   new_hash = {}
   new_hash[key] = value
+  new_hash
   # return a hash that includes the key and value parameters passed into this method
 end
 
